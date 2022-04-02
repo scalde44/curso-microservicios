74563148e940f04cab23ba99dd3f6ddd4a49915e
steven.calderon <steven.calderon@ceiba.com.co>
2022-04-02 16:19:14 -0500

    Configuracion ribbon, feign

--- servicio-items/src/main/java/com/formacionbdi/app/items/ServicioItemsApplication.java
+++ servicio-items/src/main/java/com/formacionbdi/app/items/ServicioItemsApplication.java
@@ -2,12 +2,16 @@ package com.formacionbdi.app.items;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@RibbonClient(name = "servicio-productos")
+@EnableFeignClients
 @SpringBootApplication
 public class ServicioItemsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServicioItemsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServicioItemsApplication.class, args);
+    }
 
 }
