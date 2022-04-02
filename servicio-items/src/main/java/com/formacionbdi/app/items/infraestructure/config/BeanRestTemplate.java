5526125622b825fcca4c41a26c999e4d2bb4374c
steven.calderon <steven.calderon@ceiba.com.co>
2022-04-02 16:19:46 -0500

    Configuracion load balancer rest template

--- servicio-items/src/main/java/com/formacionbdi/app/items/infraestructure/config/BeanRestTemplate.java
+++ servicio-items/src/main/java/com/formacionbdi/app/items/infraestructure/config/BeanRestTemplate.java
@@ -1,5 +1,6 @@
 package com.formacionbdi.app.items.infraestructure.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -7,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class BeanRestTemplate {
     @Bean(name = "clienteRest")
+    @LoadBalanced
     public RestTemplate registrarRestTemplate() {
         return new RestTemplate();
     }
